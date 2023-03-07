4fffa24d1499a5397eeaa18a7709f1c7e353ccf8
anushkapeer <126355742+anushkapeer@users.noreply.github.com>
2023-03-06 19:04:33 -0800

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,11 +1,18 @@
 import streamlit as st
 import pandas as pd
 
-
+@st.cache_data
 def get_MetaData():
 	df_meta = pd.read_csv("Data/df_meta.csv")
 	return df_meta
 
+@st.cache_data(ttl=600)
 def get_GeneData():
-	df_genes = pd.read_csv("Data/df_genes.csv")
+	df_genes = load_data(st.secrets["public_gsheets_url"]) # pd.read_csv("Data/df_genes.csv")
 	return df_genes
+
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
+
+
